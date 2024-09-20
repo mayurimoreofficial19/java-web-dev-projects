4e91de59326fe5d0622fa6335c66686fb04e1c8d
mayurimoreofficial19 <mayurimoreofficial19@gmail.com>
2024-09-19 20:06:32 -0500

    studio-1 completed

--- datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
+++ datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
@@ -1,4 +1,28 @@
+
 package org.launchcode;
 
+import java.util.Scanner;
+
 public class Area {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter a radius ");
+        double userInput = input.nextDouble();
+
+
+        do {
+            if (userInput < 0) {
+                System.out.println("Enter a radius ");
+                userInput = input.nextDouble();
+            }
+            else {
+                break;
+            }
+        }while(true);
+        double areaOfCircle = Circle.getArea(userInput);
+        System.out.println("The area of a circle with a radius of " + userInput + " is: " + areaOfCircle);
+
+
+    }
 }
---
+++ datatypes/datatypes-studio/src/main/java/org/launchcode/Circle.java
@@ -0,0 +1,14 @@
+package org.launchcode;
+
+import java.util.Scanner;
+
+public class Circle {
+    public static Double getArea(Double radius) {
+        return 3.14 * radius * radius;
+        // ask user a question
+        // accept user input
+        // output string with user input
+
+    }
+
+}
