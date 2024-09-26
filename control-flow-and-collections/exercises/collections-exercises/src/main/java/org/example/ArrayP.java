a75e8ae49c7824f05f5c7f18d4e541c1c01778c6
mayurimoreofficial19 <mayurimoreofficial19@gmail.com>
2024-09-25 23:33:56 -0500

    collection-exercises

--- control-flow-and-collections/exercises/collections-exercises/src/main/java/org/example/ArrayP.java
+++ control-flow-and-collections/exercises/collections-exercises/src/main/java/org/example/ArrayP.java
@@ -1,5 +1,28 @@
 package org.example;
-import java.util.Scanner;
+import java.util.Arrays;
 
 public class ArrayP {
+    public static void main(String[] args) {
+        int[] integerArray = {1, 1, 2, 3, 5, 8};
+
+        System.out.println("All values:");
+        for(int index: integerArray)
+        {
+            System.out.println(index);
+        }
+
+        System.out.println("Odd Values : ");
+        for(int value : integerArray) {
+            if(value % 2 == 1) {
+                System.out.println(value);
+            }
+        }
+
+        String phrase = "I would not, could not, in a box. I would not, could not with a fox. I will not eat them in a house. I will not eat them with a mouse";
+        String[] words = phrase.split(" ");
+        System.out.println(Arrays.toString(words));
+
+        String[] sentence = phrase.split("\\.");
+        System.out.println(Arrays.toString(sentence));
+    }
 }
